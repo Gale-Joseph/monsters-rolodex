64d7bd83c6e5193398e962ced47ca94a44147d9f
Joseph Gale <gal16026@byui.edu>
2020-07-03 00:58:14 -0500

    Updates

---
+++ precache-manifest.5b4637ce5cc4896b85e4b13918319682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2834db4f181825995968383be2e667",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4a76807004af17483dd",
+    "url": "/monsters-rolodex/static/css/main.de7f1bc4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a76807004af17483dd",
+    "url": "/monsters-rolodex/static/js/main.ffa14dfd.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
